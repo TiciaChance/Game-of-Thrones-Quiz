dfd849886bb0c524a20529c5d24bffc695a9322c
TiciaChance <laticia.chance@flatironschool.com>
2016-11-30 18:21:24 -0500

    ui including constraints done

--- Watch the Throne/ViewController.swift
+++ Watch the Throne/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func answerButtonTapped(_ sender: AnyObject) {
     }
 
     override func didReceiveMemoryWarning() {
