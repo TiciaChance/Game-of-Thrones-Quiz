fa4e6c9b0bc06854b9071366cf0d1bba39f25cc9
TiciaChance <laticia.chance@flatironschool.com>
2016-12-03 08:26:01 -0500

    removed 2nd vc, fixed last question bug, added result label

--- Watch the Throne/ViewController.swift
+++ Watch the Throne/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Social
+
 
 class ViewController: UIViewController {
     
@@ -14,12 +16,26 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var resultLabel: UILabel!
     //practice dummy data
-    let questions = ["Where are you traveling to in 2017?", "Favorite snack?", "Fav movie?", "fav british series"]
-    let answers = [["New York", "Cuba", "Japan", "Sweden"], ["Chips?", "Popcorn?", " Peanuts", "Bell Peppers"], ["Do the right thing", "all about eve", "love actually", "the pianist"], ["luther", "chewing gum", "misfits", "footballer's wives"]]
+    let questions = ["Which household possesses the banner of a moon and a falcon??",
+                     "The phrase \"Hold the door\" is associated with which character?",
+                     "What was the name of Catelyn Stark's father?",
+                     "What was the name of Arya's direwolf?",
+                     "Who was the first to stab Jon Snow in the Season 5 finale?",
+                     "This (possibly) royal character hasn't been seen since Season 3?",
+                     "Who said, \"A very small man can cast a very large shadow\"?"
+                     ]
+    let answers = [["House Arryn", "House Greyjoy", "House Lannister", "House Karstark"],
+                   ["Hodor", "Jon Snow", "Cersei", "Lord Baelish"],
+                   ["Lord Hoster", "Lord Arryn", "Lord Tywin", "Lord Edmure"],
+                   ["Nymeria", "Summer", "Snow", "Lady"],
+                   ["Alliser Thorne", "Olly", "Tormund", "Bowen Marsh"],
+                   ["Gendry", "Joffry", "Margaery", "Jon Snow"],
+                   ["Lord Varys","Tyrion","Jamie Lannister","Ned Stark"]
+                   ]
     
     var currentQuestion = 0
     var rightAnswerPlacement: UInt32 = 0
-    var point = 0
+    //var points = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,12 +47,9 @@ class ViewController: UIViewController {
     
     @IBAction func answerButtonTapped(_ sender: AnyObject) {
         
-        if sender.tag == Int(rightAnswerPlacement) {
-            questionLabel.text = "Correct!"
-            point += 1
-        } else {
+        if sender.tag != Int(rightAnswerPlacement) {
             resultLabel.isHidden = false
-            resultLabel.text = "You "
+            resultLabel.text = "You know nothing, Jon Snow. Try again!"
         }
         
         if currentQuestion != questions.count && sender.tag == Int(rightAnswerPlacement){
@@ -44,8 +57,9 @@ class ViewController: UIViewController {
         }
         
         if currentQuestion == questions.count && sender.tag == Int(rightAnswerPlacement) {
-            performSegue(withIdentifier: "showImageVC", sender: self)
-            
+           // performSegue(withIdentifier: "showImageVC", sender: self)
+            questionLabel.text = "Winter is here"
+//            resultLabel.isHidden = false
         }
     }
     
@@ -80,5 +94,3 @@ class ViewController: UIViewController {
     }
     
 }
-
-
